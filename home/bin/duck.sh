5b2251b8018f7ba7872cfcaa69244c63957bd780
Jacek Królikowski <nietaki@gmail.com>
2020-01-25 14:21:56 +0100

    Add duck.sh to bin

---
+++ home/bin/duck.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+echo "quack!"
+# stolen from https://github.com/davidjb/update-dynamic-dns/blob/master/update-duckdns.sh
+
+if [ "$1" = "--help" ]; then
+    echo "Usage: $0 domain token"
+    echo "Updates Duck DNS with an IP address for a given domain."
+    echo "Omitting the interface causes Duck DNS to determine the public IP of your location."
+    exit
+fi
+
+DOMAIN=$1
+TOKEN=$2
+
+if [ -z "$DOMAIN" ] || [ -z "$TOKEN" ]; then
+    echo "Both domain and token are required. Aborting."
+    exit 1;
+fi
+
+curl "https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip="
