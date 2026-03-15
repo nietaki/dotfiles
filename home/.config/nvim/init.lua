f4fc21fcf96c570f82cd944580fd6a0568f6a97d
Jacek Królikowski <nietaki@gmail.com>
2026-03-15 21:23:38 +0100

    TODO notes

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,38 +1,51 @@
 vim.cmd 'runtime! vimscript/migration_utils.vim'
--- 1
 vim.cmd 'runtime! vimscript/compat.vim'
--- 2
 vim.cmd 'runtime! vimscript/pre_plugin_config.vim'
 
--- 3
 require('lazy_setup')
 
--- 4
 vim.cmd 'runtime! vimscript/post_plugin_keymaps.vim'
 
--- 5
 vim.cmd 'runtime! vimscript/search_config.vim'
 
--- 6
 vim.cmd 'runtime! vimscript/git_stuff.vim'
 
 require('workspace_stuff')
 require('lsp_setup')
 
--- 9
 vim.cmd 'runtime! vimscript/filetype_specific.vim'
 
 require('treesitter_setup')
 
--- 11
 vim.cmd 'runtime! vimscript/leftovers.vim'
 
 local ok, _ = pcall(require, 'experimental')
 if not ok then
   print('Experimental module failed to load')
 end
-
-
-
+-- # backup plan :D
 -- vim.cmd 'runtime! old.init.vim'
 
+
+--[[
+
+## Gameplan:
+
+- [x] break down the old config into pieces
+- [ ] go through the files and purge all the old keymaps and plugins - it's safe if we have a nice git history and the old config as reference
+- [ ] set up barebones nvim-cmp
+- [ ] plug in one lsp server into nvim-cmp
+
+
+## long tail
+- choose a vim fragment
+- translate to lua
+- ???
+- PROFIT
+
+## wishlist
+
+- [ ] break down the plugins config into files (there's a way that I don't understand yet)
+- [ ] fix luarocks
+
+]]
