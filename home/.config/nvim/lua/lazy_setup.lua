94ecdc37db5a44487801718d5f75ce8c2905f1f7
Jacek Królikowski <nietaki@gmail.com>
2026-03-15 22:07:07 +0100

    fight lazy nvim

--- home/.config/nvim/lua/lazy_setup.lua
+++ home/.config/nvim/lua/lazy_setup.lua
@@ -22,6 +22,34 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
 {
+  ui = {
+    -- still stupid, but still better than nerdfonts
+    icons = {
+      cmd = '⌘',
+      config = '🛠',
+      debug = "[dbg]",
+      event = '📅',
+      ft = '📂',
+      import = "[imp]",
+      init = '⚙',
+      keys = '🗝',
+      lazy = '💤 ',
+      loaded = "●",
+      not_loaded = "○",
+      plugin = '🔌',
+      require = '🌙',
+      runtime = '💻',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      list = {
+        "●",
+        "➜",
+        "★",
+        "‒",
+      },
+    },
+  },
   rocks = {
     enabled = true,
     -- for now, because :checkhealth lazy says my luarocks install is broken, and I can't be bothered
@@ -47,16 +75,15 @@ require("lazy").setup(
     {"tpope/vim-abolish"},
 
     {"nvim-lua/plenary.nvim"},
-    {"dart-lang/dart-vim-plugin"},
-    {"akinsho/flutter-tools.nvim"},
 
     --{"townk/vim-autoclose"},
-    {"terryma/vim-multiple-cursors"},
+    -- {"terryma/vim-multiple-cursors"},
     {"nelstrom/vim-visual-star-search"},
 
     {"janko/vim-test"},
     {"tpope/vim-dispatch"},
     {"tpope/vim-eunuch"},
+
     --{"tpope/vim-surround"},
     { 'echasnovski/mini.surround', version = '*' },
     { 'echasnovski/mini.clue', version = '*' },
@@ -67,63 +94,15 @@ require("lazy").setup(
     {"joshdick/onedark.vim"},
     {"tomasr/molokai"},
     {"srcery-colors/srcery-vim", lazy = false},
+    -- TODO taboo is unmaintained, figure out why I added it and replace with something else
     {"gcmt/taboo.vim"},
-
-    -- experimental
-    {"tpope/vim-rails"},
 
     {"nvim-treesitter/playground"},
 
-    -- {"neovim/nvim-lspconfig"},
-    -- {"hrsh7th/cmp-path"},
-    -- {"hrsh7th/cmp-buffer"},
     {'simrat39/symbols-outline.nvim'},
-    --{'hrsh7th/nvim-cmp',
-    --  dependencies = {
-    --    {'hrsh7th/cmp-nvim-lsp'},
-    --    {"hrsh7th/cmp-path"},
-    --    {"hrsh7th/cmp-buffer"},
-    --    {'L3MON4D3/LuaSnip', build = "make install_jsregexp" },
-    --  }
-    --},
-
-    --{
-    --  'VonHeikemen/lsp-zero.nvim',
-    --  branch = 'v2.x',
-    --  dependencies = {
-    --    -- LSP Support
-    --    {'neovim/nvim-lspconfig'},             -- Required
-    --    {                                      -- Optional
-    --      'williamboman/mason.nvim',
-    --      build = function()
-    --        pcall(vim.cmd, 'MasonUpdate')
-    --      end,
-    --    },
-    --    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-    --    -- Autocompletion
-    --    {'hrsh7th/nvim-cmp'},     -- Required
-    --    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    --    {'L3MON4D3/LuaSnip', build = "make install_jsregexp" },     -- Required
-    --  }
-    --},
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  --  {
-  --    "folke/which-key.nvim",
-  --    config = function()
-  --      --vim.o.timeout = true
-  --      --vim.o.timeoutlen = 300
-  --      require("which-key").setup({
-  --        -- your configuration comes here
-  --        -- or leave it empty to use the default settings
-  --        -- refer to the configuration section below
-  --      })
-  --    end,
-  --  },
-
-    {
-        {"Cassin01/wf.nvim", version = "*", config = function() require("wf").setup() end}
-    },
+    -- which-key, needs configuration
+    -- {"Cassin01/wf.nvim", version = "*", config = function() require("wf").setup() end},
     {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
       dependencies = { 'nvim-lua/plenary.nvim' }
@@ -131,26 +110,7 @@ require("lazy").setup(
     {"ahmedkhalf/project.nvim"},
     {'nvim-telescope/telescope-fzf-native.nvim', build = "make" },
     { "lukas-reineke/indent-blankline.nvim" },
-    {
-      "S1M0N38/love2d.nvim",
-      cmd = "LoveRun",
-      opts = { },
-      keys = {
-        { "<leader>v", desc = "LÖVE" },
-        { "<leader>vv", "<cmd>LoveRun<cr>", desc = "Run LÖVE" },
-        { "<leader>vs", "<cmd>LoveStop<cr>", desc = "Stop LÖVE" },
-      },
-    },
     {'github/copilot.vim'},
     {'fatih/vim-go'},
-  --  {
-  --    "greggh/claude-code.nvim",
-  --    dependencies = {
-  --      "nvim-lua/plenary.nvim", -- Required for git operations
-  --    },
-  --    config = function()
-  --      require("claude-code").setup()
-  --    end
-  --  }
   }
 })
